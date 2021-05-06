57669f1f0e5e112add164e178b06a12319406314
Viramat <virasisombat@yahoo.fr>
2021-05-06 16:46:53 +0200

    Modification Modele.php et Top.php

--- modeles/Modele.php
+++ modeles/Modele.php
@@ -1,7 +1,6 @@
 <?php
-class Modele
-    {
-        protected function getBdd(){
-            return new PDO('mysql:host=localhost;dbname=tp_quizz', 'root', '', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-        }
+class Modele{
+    protected function getBdd(){
+        return new PDO('mysql:host=localhost;dbname=tpquizz;charset=UTF8', 'root', '', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
     }
+}
--- modeles/Top.php
+++ modeles/Top.php
@@ -3,7 +3,7 @@ class Top extends Modele{
     private $topCategorie = [];
 
     public function initialiserTopCategorie(){
-        $requete = $this->getBdd()->prepare ("SELECT idCategorie,categories.titre,photo,COUNT(idQuizz) as nbrQuizz FROM categories LEFT JOIN quizz USING(idCategorie) GROUP BY idCategorie ORDER BY nbrQuizz DESC LIMIT 4");
+        $requete = $this->getBdd()->prepare ("SELECT IdCategorie,titre,photo,COUNT(idQuizz) as nbrQuizz FROM categories LEFT JOIN quizz USING(idCategorie) GROUP BY IdCategorie ORDER BY nbrQuizz DESC LIMIT 4");
         $requete->execute();
         $top = $requete->fetchAll(PDO::FETCH_ASSOC);
         foreach($top as $categorie){
